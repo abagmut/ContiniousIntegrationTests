75b4f1415df69ad878d4157153b33cb2c4119246
abagmut <abagmut@prophonix.com>
2016-10-11 23:22:01 +0300

    initial commit

---
+++ IntegrationTests/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  IntegrationTests
+//
+//  Created by Alex on 11.10.2016.
+//  Copyright © 2016 Prophonix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	internal func returnTrue() -> Bool
+	{
+//		return false
+		return true
+	}
+}
+
